f5b4cca3ee100c0348fe8abf7a13701667d96acf
dwass <dwass@sharpmind.de>
2020-09-25 14:18:52 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -229,7 +229,7 @@ namespace BME680 {
         P = _p + ((var1 + var2 + dig_P7) >> 4)
     */
         // Get humidity value
-        let hum_adc = getInt16LE(0xE5)
+        let hum_adc = getInt16LE(0x25)
         // Convert humidity value
         let temp_scaled = temp_comp
         var1 = hum_adc - (par_h1 << 4) - (((temp_scaled * par_h3) / 100) >> 1)
